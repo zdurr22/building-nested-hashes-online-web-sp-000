c977176ecd83c04165af801dc6593de0e21acac5
Zachary Durr <zdurr22@gmail.com>
2020-10-12 22:11:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -21,10 +21,10 @@ def second_challenge
       :matriarch => {
 
         },
-      :hero => {
+      :heroine => {
 
         },
-      :hero_friends = [
+      :heroine_friends = [
 
         ]
     }
