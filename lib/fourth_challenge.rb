ac2ee72f83d455e5d489cc9aae412ca50e782730
Zachary Durr <zdurr22@gmail.com>
2020-10-12 22:20:01 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,19 +5,22 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        :first_friend => {name: "Benvolio", age: "17", attitude: "worried"},
+        :second_friend => {name: "Mercutio", age: "18", attitude: "hot-headed"}
+
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        :first_friend => {name: "Steven", age: "30", attitude: "confused"},
+        :second_friend => {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
 
-  
+
 
 end
